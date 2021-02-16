6bcf2fb1facc0e8a1f2b4afca973d000ff8b1b9d
GuilhermeGCS-BOT15 <79100010+GuilhermeGCS-BOT15@users.noreply.github.com>
2021-02-16 07:33:22 -0300

    Add files via upload

---
+++ Komputer/suporte.java
@@ -0,0 +1,5 @@
+Pc = komputer * (GCS-BOT3.0 + GCS-BOT - GCS)
+           + 4 * bot; // PREFER
+
+Android = /0/android/suporte.java * (GCS-BOT3.0 + GCS
+                       - GCS-BOT) + 4 * bot; // AVOID
