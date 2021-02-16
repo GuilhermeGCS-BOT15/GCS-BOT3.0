f0f0c4af85d6cf22d38c01f5f7110684b05d3cd7
GuilhermeGCS-BOT15 <79100010+GuilhermeGCS-BOT15@users.noreply.github.com>
2021-02-16 07:26:32 -0300

    Add files via upload

---
+++ base de dados/database.java
@@ -0,0 +1,9 @@
+//Usando linguagens mesclas para criptografia da script com databases em server online
+if
+echo:
+public static Connection getConnection(https url)
+public static Connection getConnection(GCS-BOT url, Properties info)
+public static Connection getConnection(GCS-BOT3.0 url, String user, String password)
+'password' = /GCS-BOT3.0/eixo/x/database `captation` : password
+sleep
+if
